42328ef45e1b57e1b635dbe8c065236d0a4534b7
qqbeach <danildzekelev@gmail.com>
2023-12-21 13:28:18 +0200

    Rename compliance to conformity. Add non_conformity_area func.

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #import matplotlib.pyplot as plt
 import math
+
 def compliance_area(r, a, b, t_0):
 
     ln = math.log(b * (1 - a)**-1)
 
-
-
     k = ((n * (r - 1)) / (t_0 * math.log(r))) 
-
     k_1 = ln / math.log(r)
 
-    k_lower = str(k) + "t" + str(k_1) 
+    k_lower = str(k) + " t " + str(k_1) 
     return k_lower
+
 r = 1.5 # 1.5
 a = 0.1 # 0.05
 b = 0.1 # 0.05
@@ -24,7 +23,7 @@ h_1 = 24.43 # 36.74
 M_T0 = (h_0 * t_0) / n
 M_T1 = (h_1 * t_1) / n
 
-compliance_area(r, a, b, t_0)
+compliance = compliance_area(r, a, b, t_0)
 
 
 
